531178281490e7dc4e4becac567e16940ed6c14f
Nick Dean <klick5000@gmail.com>
2018-10-10 22:38:55 -0600

    added pseudo-code to js file

---
+++ index.js
@@ -0,0 +1,21 @@
+// Create the search URL
+// const YOUTUBE_SEARCH_URL = '';
+
+// Watch for a listener on the Submit button
+// function watchSubmit()
+// is going to prevent default
+// get the query Target and pull the value from it
+// Clear the input
+// Send the query to the API and set a callback
+
+// Callback displayYoutubeSearchData will need to
+// map each data.items and show each result
+
+// When you send query to API you are getting the Data from the API
+// function getDataFromApi(searchTerm, callback)
+// You need to set up a query based on your searchTerm
+// then call $.getJSON(url, query, callback);
+
+// show each result using renderResult(result)
+// result is each item result
+// We will be returning the HTML
